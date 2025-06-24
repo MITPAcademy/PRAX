7209691e08432441fa918f87da74420bb21a69e8
I'm Matheus <codelong@proton.me>
2025-06-23 23:42:29 -0300

    Fixing Escape test

--- tex/escape.py
+++ tex/escape.py
@@ -1,12 +1,14 @@
 def escape_tex(text):
-    text = text.replace("\\", r"\textbackslash{}")
-    text = text.replace("&", r"\&")
-    text = text.replace("%", r"\%")
-    text = text.replace("$", r"\$")
-    text = text.replace("#", r"\#")
-    text = text.replace("_", r"\_")
-    text = text.replace("{", r"\{")
-    text = text.replace("}", r"\}")
-    text = text.replace("~", r"\textasciitilde{}")
-    text = text.replace("^", r"\^{}")
-    return text
+    mapping = {
+        "&": r"\&",
+        "%": r"\%",
+        "$": r"\$",
+        "#": r"\#",
+        "_": r"\_",
+        "{": r"\{",
+        "}": r"\}",
+        "~": r"\textasciitilde{}",
+        "^": r"\^{}",
+        "\\": r"\textbackslash{}",
+    }
+    return "".join(mapping.get(char, char) for char in text)
